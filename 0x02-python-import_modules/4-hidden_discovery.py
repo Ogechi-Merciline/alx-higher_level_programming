7be5f193cae4ead12423dd494334427bdeba686c
root <ogechimercyline@gmail.com>
2023-05-11 09:46:11 -0700

    Print all the names defined by the compiled module hidden_4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ = "__main__":
-    """Print all the names defined by the compiled module hidden_4.pyc."""
+    """Print all the names defined by the compiled module hidden_4."""
     import hidden_4
 
     names = dir(hidden_4)
