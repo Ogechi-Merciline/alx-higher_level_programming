4fdff98a0a0b1fd2df689d6e08042b002541433f
root <ogechimercyline@gmail.com>
2023-05-11 11:17:06 -0700

    Prints value of variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """imports variable a from file variable_load_5.py and prints its value."""
-    import a from variable_load_5
+    """Prints value of variable a from variable_load_5."""
+
+    from variable_load_5 import a
 
     print(a)
