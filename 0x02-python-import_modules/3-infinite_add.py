d87878783af25340a8c528a05680522c3efcf5a2
root <ogechimercyline@gmail.com>
2023-05-11 12:37:32 -0700

    imports all functions from the file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """prints the result of the addition of all arguments."""
+    """print the addition of all arguments."""
     import sys
 
     total = 0
