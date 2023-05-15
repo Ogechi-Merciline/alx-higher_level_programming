98b986778534db464f46e7925899aa0d72a77e9b
root <ogechimercyline@gmail.com>
2023-05-15 12:34:08 -0700

    Prints all intergers of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     """Print all intergers of a list."""
     for i in range(len(my_list)):
-        print("{:d".format(my_list[i]))
+        print("{:d]".format(my_list[i]))
