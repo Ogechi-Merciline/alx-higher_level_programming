959a94a5fef064f2135f1713993e0837099e41cd
Ogechi-Merciline <ogechimercyline@gmail.com>
2023-06-05 10:13:08 -0700

    Compare Rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
-    print symbol = "#"
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
