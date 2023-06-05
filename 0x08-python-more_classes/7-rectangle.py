900a70404679ad69271b7523e41000e34d7c4388
Ogechi-Merciline <ogechimercyline@gmail.com>
2023-06-05 09:43:38 -0700

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,12 +6,11 @@ class Rectangle:
     """Represent a rectangle.
 
     Attributes:
-        number_of_instances (int): The number of rectangle instances.
-        print_symbol (any): The symbol used for string representation.
+        number_of_instances (int): The number of Rectangle instances.
     """
 
     number_of_instances = 0
-    print symbol = "#"
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -59,22 +58,6 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Return the Rectangle with the greater area.
-
-        Args:
-            rect_1 (Rectangle): The first Rectangle.
-            rect_2 (Rectangle): the second rectangle.
-        Raises:
-            TypeError: If either of rect_1 or erect_2 is not a Rectangle.
-        """
-        if not isisntance(rect_1, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.are() >= rect_2.area():
-            return (rect_1)
-        return (rect_2)
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
