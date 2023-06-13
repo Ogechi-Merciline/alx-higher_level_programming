8ddc52f5f3a52f8af742e265a548d473e4dd3dd1
Ogechi-Merciline <ogechimercyline@gmail.com>
2023-06-13 09:23:58 -0700

    To JSON function

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""Defines a file writiong function."""
+"""Defines a string-to-JSON functions."""
+import json
 
 
 def to_json_string(my_obj):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write
-        text (str): The text to write to the file.
-    returns:
-        The number of characterswritten.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
