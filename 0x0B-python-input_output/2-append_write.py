d678beaac6c80484442f3628f8a1507cd1698a9a
Ogechi-Merciline <ogechimercyline@gmail.com>
2023-06-13 08:52:54 -0700

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def append_write(filename="", text=""):
+    """Print a given number of lines from a UTF8 text file to stdout.
+    Args:
+        filename (str): The name of the file
+        nb_lines (int): The number of lines to read from file.
+    """
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+            return
+
+        lines = 0
+        for line in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines >= lines:
+            print(f.readline(), end="")
+            n += 1
